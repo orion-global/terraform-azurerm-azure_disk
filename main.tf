10d0c51f94fdbe185de30af89dd071e4e5e64396
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-12-13 01:50:21 +0100

    Initialization
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

---
+++ main.tf
@@ -0,0 +1,43 @@
+#------------------------------------------------------------------------------------------
+# Local Variables
+#------------------------------------------------------------------------------------------
+
+locals {
+}
+
+
+#------------------------------------------------------------------------------------------
+# Resource Group
+#------------------------------------------------------------------------------------------
+
+resource "azurerm_resource_group" "resource_group" {
+  count    = var.create_resource_group ? 1 : 0
+  name     = var.resource_group_name
+  location = var.location_name
+  tags     = var.tags
+}
+
+data "azurerm_resource_group" "resource_group" {
+  count = var.create_resource_group ? 0 : 1
+  name  = var.resource_group_name
+}
+
+#------------------------------------------------------------------------------------------
+# Disk Configuration
+#------------------------------------------------------------------------------------------
+
+resource "azurerm_managed_disk" "example" {
+  name                 = "${local.vm_name}-disk1"
+  location             = azurerm_resource_group.example.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.example.id
+  virtual_machine_id = azurerm_virtual_machine.example.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
